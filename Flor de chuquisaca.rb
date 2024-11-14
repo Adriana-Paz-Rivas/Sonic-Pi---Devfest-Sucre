5b20466952693d8b9f8395afc73cf258031cc476
Adriana-Paz-Rivas <160977118+Adriana-Paz-Rivas@users.noreply.github.com>
2024-11-14 15:18:23 -0400

    Add files via upload

---
+++ Flor de chuquisaca.rb
@@ -0,0 +1,81 @@
+# Flor de chuquisaca
+use_bpm 120  #beats por minuto cueca
+
+#live_loop :melody do
+ # synth :piano
+  #play_pattern_timed [:d4, :c4 ],
+  #  [2, 2 ]
+end
+2. times do
+  #Primera parte
+  synth :piano
+  play 66
+  sleep 0.5
+  play 65
+  sleep 0.5
+  play 66
+  sleep 0.5
+  play 68
+  sleep 0.5
+  play 69
+  sleep 1
+  play 66
+  sleep 3
+  
+  
+  #Segunda parte
+  play 66
+  sleep 0.5
+  play 69
+  sleep 0.5
+  2. times do
+    play 64
+    sleep 0.5
+  end
+  play 63
+  sleep 1
+  play 61
+  sleep 3
+  
+  #Tercera parte
+  
+  play 62
+  sleep 0.5
+  play 66
+  sleep 0.5
+  play 65
+  sleep 0.5
+  play 68
+  sleep 1
+  play 66
+  sleep 0.5
+  play 61
+  sleep 0.5
+  play 64
+  sleep 0.5
+  play 63
+  sleep 0.5
+  play 66
+  sleep 1
+  play 64
+  sleep 0.5
+  play 68
+  sleep 0.5
+  play 71
+  sleep 0.5
+  play 70
+  sleep 0.5
+  play 61
+  sleep 1
+  play 71
+  sleep 0.5
+  play 69
+  sleep 2
+  
+  
+  
+  
+end
+
+
+
